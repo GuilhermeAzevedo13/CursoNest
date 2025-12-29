60df1d37f32f38759beeeb70ec7af62195358044
Guilherme Menezes de Azevedo <guilhermemenezes20102010@hotmail.com>
2025-12-29 16:16:12 -0300

    aula 21

--- src/recados/recados.controller.ts
+++ src/recados/recados.controller.ts
@@ -5,9 +5,21 @@ import {
   HttpCode,
   HttpStatus,
   Param,
+  Patch,
   Post,
+  Put,
 } from '@nestjs/common';
 import { RecadosService } from './recados.service';
+
+// CRUD
+// Create -> POST -> Criar um recado
+// Read -> GET -> Ler todos os recados
+// Read -> GET -> Ler apenas um recado
+// Update -> PATCH / PUT -> Atualizar um recado
+// Delete -> DELETE -> Apagar um recado
+
+// PATCH é utilizado para atualizar dados de um recurso
+// PUT é utilizado para atualizar um recurso inteiro
 
 @Controller('recados')
 export class RecadosController {
@@ -30,5 +42,13 @@ export class RecadosController {
   create(@Body() body: any) {
     console.log(body);
     return `Essa rota cria um recado`;
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() body: any) {
+    return {
+      id,
+      ...body,
+    };
   }
 }
