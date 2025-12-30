e73cf0d979d76f5b904aec227a6e5a398fb03ef0
Guilherme Menezes de Azevedo <guilhermemenezes20102010@hotmail.com>
2025-12-30 16:30:39 -0300

    adicionando coisas da aula 28

--- src/recados/dto/create-recados.dto.ts
+++ src/recados/dto/create-recados.dto.ts
@@ -1,7 +1,30 @@
+import {
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
 export class CreateRecadosDto {
   //id: number; //Nao preciso disso pois eh gerado pela base de dadoos.
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(1)
+  @MaxLength(255)
+  @IsOptional()
   readonly texto: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(3)
+  @MaxLength(12)
   readonly de: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(3)
+  @MaxLength(12)
   readonly para: string;
   //lido: boolean;
   //data: Date;
